61f3639137d77156275ec9ecbeca160427220204
iaajl <iamaravindjl@gmail.com>
2024-05-23 11:00:00 +0530

    second

---
+++ 14/ground.py
@@ -0,0 +1,15 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score,precision_recall_fscore_support
+from sklearn.metrics import f1_score,roc_auc_score
+from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
+from sklearn.tree import DecisionTreeClassifier
+import xgboost as xgb
+from xgboost import plot_importance
